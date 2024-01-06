9eb3c10e83ac9d10938410c5cf081248a7127705
KilakOriginal <dev@kilakoriginal.site>
2024-01-06 02:17:14 +0100

    Update scripts

---
+++ README.md
@@ -0,0 +1,27 @@
+# arch-config
+## About this project
+This is my Arch Linux configuration. I use the following software:
+
+> [!WARNING]
+> This list is incomplete! I will also be adding all the plugins I'm using to it soon.
+
+- [Wayland](https://wayland.freedesktop.org/)
+- [Hyprland](https://hyprland.org/)
+- [Hyprpaper](https://github.com/hyprwm/hyprpaper)
+- [Hyprshot](https://github.com/Gustash/Hyprshot)
+- [Waybar](https://github.com/Alexays/Waybar)
+- [Pyprland](https://pypi.org/project/pyprland/)
+- [Neovim](https://neovim.io/)
+- [WezTerm](https://wezfurlong.org/wezterm/)
+- [kitty](https://sw.kovidgoyal.net/kitty/)
+- [Nemo](https://github.com/linuxmint/nemo)
+- [Swaylock-effects](https://github.com/mortie/swaylock-effects)
+- [SDDM](https://github.com/sddm/sddm)
+- [Oh My Zsh](https://ohmyz.sh/)
+- [Wofi](https://sr.ht/~scoopta/wofi/)
+- [wlogout](https://github.com/ArtsyMacaw/wlogout)
+
+> [!CAUTION]
+> The install script will overwrite any symlinks already present!
+
+To install the configuration files simply run the install script or manually create the symlinks.
--- config/scripts/lock.sh
+++ config/scripts/lock.sh
@@ -1,10 +1,8 @@
 #/bin/sh
 
-screen="50%"
-keyboard=1
-
-swayidle -w timeout 295 "screen=$(brightnessctl get) && brightnessctl set 1%" \
-	resume "brightnessctl set $screen" \
-timeout 300 "swaylock -f && hyprctl dispatch dpms off && keyboard=$(brightnessctl --device='platform::kbd_backlight' get) && brightnessctl --device='platform::kbd_backlight' s 0" \
-	resume "hyprctl dispatch dpms on && brightnessctl set $screen && brightnessctl --device='platform::kbd_backlight' s $keyboard" \
-timeout 600 'systemctl suspend'
+swayidle -w timeout 295 'echo screen=$(brightnessctl get) > /tmp/brightness_levels && brightnessctl set 1%' \
+	resume 'source /tmp/brightness_levels ; brightnessctl set $screen' \
+timeout 300 'swaylock -f && hyprctl dispatch dpms off && echo keyboard=$(brightnessctl --device="platform::kbd_backlight" get) >> /tmp/brightness_levels && brightnessctl --device="platform::kbd_backlight" s 0' \
+	resume 'source /tmp/brightness_levels ; hyprctl dispatch dpms on && brightnessctl set $screen && brightnessctl --device="platform::kbd_backlight" s $keyboard' \
+timeout 600 'systemctl suspend' \
+	resume 'source /tmp/brightness_levels ; hyprctl dispatch dpms on && brightnessctl set $screen && brightnessctl --device="platform::kbd_backlight" s $keyboard' 
