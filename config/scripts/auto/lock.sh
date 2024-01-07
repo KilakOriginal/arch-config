36af5009a0cec949b47ed895898a407d726b8437
KilakOriginal <dev@kilakoriginal.site>
2024-01-07 18:10:55 +0100

    Fix auto locker

--- config/scripts/auto/lock.sh
+++ config/scripts/auto/lock.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 
-swayidle -w before-sleep 'swaylock -f'
+swayidle -w before-sleep 'swaylock -f' \
 timeout 295 'echo screen=$(brightnessctl get) > /tmp/brightness_levels && brightnessctl set 1%' \
 	resume 'source /tmp/brightness_levels ; brightnessctl set $screen' \
 timeout 300 'swaylock -f && hyprctl dispatch dpms off && echo keyboard=$(brightnessctl --device="platform::kbd_backlight" get) >> /tmp/brightness_levels && brightnessctl --device="platform::kbd_backlight" s 0' \
