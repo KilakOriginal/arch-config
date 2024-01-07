4354e783ab24c80dcc12b0a497639583a97b4d89
KilakOriginal <dev@kilakoriginal.site>
2024-01-07 20:09:44 +0100

    Auto locker bug, Shortcut

--- config/scripts/auto/lock.sh
+++ config/scripts/auto/lock.sh
@@ -5,5 +5,5 @@ timeout 295 'echo screen=$(brightnessctl get) > /tmp/brightness_levels && bright
 	resume 'source /tmp/brightness_levels ; brightnessctl set $screen' \
 timeout 300 'swaylock -f && hyprctl dispatch dpms off && echo keyboard=$(brightnessctl --device="platform::kbd_backlight" get) >> /tmp/brightness_levels && brightnessctl --device="platform::kbd_backlight" s 0' \
 	resume 'source /tmp/brightness_levels ; hyprctl dispatch dpms on && brightnessctl set $screen && brightnessctl --device="platform::kbd_backlight" s $keyboard' \
-timeout 600 'systemctl suspend' \
-	resume 'source /tmp/brightness_levels ; hyprctl dispatch dpms on && brightnessctl set $screen && brightnessctl --device="platform::kbd_backlight" s $keyboard' 
+timeout 600 'hyprctl dispatch dpms on && systemctl suspend' \
+	resume 'source /tmp/brightness_levels ; brightnessctl set $screen && brightnessctl --device="platform::kbd_backlight" s $keyboard' 
