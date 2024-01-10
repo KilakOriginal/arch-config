b000eeaebac1a65dc4bd33be76446330d2b9b3f2
KilakOriginal <dev@kilakoriginal.site>
2024-01-10 21:38:55 +0100

    Screen mirror, wvkbd toggle

---
+++ config/scripts/toggle_wvkbd.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+
+state='off'
+
+if [ $state = 'on' ]; then
+    sed -i 's/"modules-left": \["hyprland\/workspaces", "custom\/wvkbd"\]/"modules-left": \["hyprland\/workspaces"\]/g' ~/.config/waybar/config \
+    && sed -i "s/^state='on'$/state='off'/g" ~/.config/scripts/toggle_wvkbd.sh
+else
+    sed -i 's/"modules-left": \["hyprland\/workspaces"\]/"modules-left": \["hyprland\/workspaces", "custom\/wvkbd"\]/g' ~/.config/waybar/config \
+    && sed -i "s/^state='off'$/state='on'/g" ~/.config/scripts/toggle_wvkbd.sh
+fi
+
+hyprctl dispatch 'exec killall waybar && waybar & disown'
