589316c80c6b496bca5454c17a196a91e349a63d
KilakOriginal <dev@kilakoriginal.site>
2024-01-20 21:12:21 +0100

    Wallpaper

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 mkdir -p ~/.config
 
-ln -sf ${PWD}/user/. $HOME
-ln -sf ${PWD}/config/. $HOME/.config
-ln -sf ${PWD}/assets/Wallpapers/. $HOME/Pictures/Wallpapers
+ln -sf ${PWD}/user/.* $HOME
+ln -sf ${PWD}/user/* $HOME
+ln -sf ${PWD}/config/* $HOME/.config
+ln -sf ${PWD}/assets/Wallpapers/* $HOME/Pictures/Wallpapers
