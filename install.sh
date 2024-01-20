ec7c3e71b6e499ae8bfbaf8850457095963f2c0f
KilakOriginal <dev@kilakoriginal.site>
2024-01-20 19:01:15 +0100

    Fix install script

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 mkdir -p ~/.config
 
-ln -sf ${PWD}/user/.* $HOME
-ln -sf ${PWD}/config/* $HOME/.config
+ln -sf ${PWD}/user/. $HOME
+ln -sf ${PWD}/config/. $HOME/.config
+ln -sf ${PWD}/assets/Wallpapers/. $HOME/Pictures/Wallpapers
